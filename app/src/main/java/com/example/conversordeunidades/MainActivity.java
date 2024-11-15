e7bc44887eb59faa511fde741706b6dc6ccb98c6
jonastiago <jonastiago95@gmail.com>
2024-11-14 22:24:10 -0300

    implementação da logica de conversão

--- app/src/main/java/com/example/conversordeunidades/MainActivity.java
+++ app/src/main/java/com/example/conversordeunidades/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.conversordeunidades;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -20,14 +23,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Spinner initialSpinner = (Spinner) findViewById(R.id.input_convert_spinner);
-        Spinner finalSpinner = (Spinner) findViewById(R.id.output_convert_spinner);
+        Spinner initialSpinner = findViewById(R.id.input_convert_spinner);
+        Spinner finalSpinner = findViewById(R.id.output_convert_spinner);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this,
@@ -41,37 +45,63 @@ public class MainActivity extends AppCompatActivity {
         finalSpinner.setAdapter(adapter);
 
         initialSpinner.setSelection(1);
-        finalSpinner.setSelection(3);
+        finalSpinner.setSelection(0);
+    }
 
-   }
+    public void Converter(View view) {
+        EditText editInput = findViewById(R.id.edit_Input_value);
+        TextView valueResult = findViewById(R.id.text_result);
 
-   public void Converter(View view){
-        Spinner inputSpinner, outputSpinner;
-        TextView valueResult;
-        EditText editInput;
-        Double result;
+        if (TextUtils.isEmpty(editInput.getText())) {
+            editInput.setError("Valor não pode ser vazio");
+            return;
+        }
 
-        valueResult = findViewById(R.id.text_result);
-        editInput = findViewById(R.id.edit_Input_value);
+        double valueEditInput;
+        try {
+            valueEditInput = Double.parseDouble(editInput.getText().toString());
+        } catch (NumberFormatException e) {
+            editInput.setError("Digite um número válido");
+            return;
+        }
 
-        double valueEditInput = Double.parseDouble(editInput.getText().toString());
+        Spinner inputSpinner = findViewById(R.id.input_convert_spinner);
+        Spinner outputSpinner = findViewById(R.id.output_convert_spinner);
 
-        inputSpinner = findViewById(R.id.input_convert_spinner);
-        outputSpinner = findViewById(R.id.output_convert_spinner);
+        String valorInputSpinner = inputSpinner.getSelectedItem().toString();
+        String valorOutputSpinner = outputSpinner.getSelectedItem().toString();
 
-         var valorInputSpinner = inputSpinner.getSelectedItem();
-         var valorOutputSpinner = outputSpinner.getSelectedItem();
+        double result = realizarConversao(valueEditInput, valorInputSpinner, valorOutputSpinner);
 
-       valueResult.setText("U$: " + String.format("%.2f", valueEditInput));
+        valueResult.setText(result + " " + valorOutputSpinner);
 
-        if (valorInputSpinner.equals("Metros")) {
-            if (valorOutputSpinner.equals("Centimetros")) {
-                result = valueEditInput * 100;
-            } else if (valorOutputSpinner. equals("Quilometros")) {
-                result = valueEditInput / 1000;
-            } else if (valorOutputSpinner. equals("Milhas")) {
-                result = valueEditInput / 1609.34;
-            }
-       }
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    private double realizarConversao(double valor, String unidadeInicial, String unidadeFinal) {
+        switch (unidadeInicial) {
+            case "Centímetros":
+                if (unidadeFinal.equals("Metros")) return valor / 100;
+                if (unidadeFinal.equals("Quilômetros")) return valor / 100000;
+                if (unidadeFinal.equals("Milhas")) return valor / 160900;
+                break;
+            case "Milhas":
+                if (unidadeFinal.equals("Metros")) return valor * 1609;
+                if (unidadeFinal.equals("Quilômetros")) return valor * 1.609;
+                if (unidadeFinal.equals("Centímetros")) return valor * 160900;
+                break;
+            case "Quilômetros":
+                if (unidadeFinal.equals("Metros")) return valor * 1000;
+                if (unidadeFinal.equals("Milhas")) return valor / 1.609;
+                if (unidadeFinal.equals("Centímetros")) return valor * 100000;
+                break;
+            case "Metros":
+                if (unidadeFinal.equals("Centímetros")) return valor * 100;
+                if (unidadeFinal.equals("Quilômetros")) return valor / 1000;
+                if (unidadeFinal.equals("Milhas")) return valor / 1609;
+                break;
+        }
+        return 0.0;
     }
 }
